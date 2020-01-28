662f90a3a043b9dc68b943d2b39491618accc1d5
Raja s <rajaaltus@gmail.com>
2020-01-28 11:04:04 +0700

    Header added

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: 'nuxt-firebaseui-starter',
+    title: 'StreetWall India Pvt.Ltd - DD MALL',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
